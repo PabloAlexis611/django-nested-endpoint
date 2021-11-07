bfb2efab8143e652891cd7dad6a3860b137693fe
Pablo Alexis Dominguez Grau <pabloadominguezgrau@gmail.com>
2021-11-07 10:08:13 -0500

    Add date widgets

--- cars/forms.py
+++ cars/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.forms.widgets import DateInput
 from cars.models import Car, Manufacturer
 
 
@@ -6,9 +7,27 @@ class CarsForm(ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
-
+        widgets = {
+            'prod_date': DateInput(
+                format=('%Y-%m-%d'),
+                attrs={
+                    'placeholder': 'Select a date',
+                    'type': 'date'
+                }
+            )
+        }
 
 class ManufacturersForm(ModelForm):
+
     class Meta:
         model = Manufacturer
         fields = '__all__'
+        widgets = {
+            'creation_date': DateInput(
+                format=('%Y-%m-%d'),
+                attrs={
+                    'placeholder': 'Select a date',
+                    'type': 'date'
+                }
+            )
+        }
